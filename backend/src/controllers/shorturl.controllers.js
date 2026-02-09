c488b6f13e0f93b8e8f40bdb9ae81046b7821a50
HsrRaza <hr.raza04@gmail.com>
2026-02-09 16:56:06 +0530

    fix/tested

--- backend/src/controllers/shorturl.controllers.js
+++ backend/src/controllers/shorturl.controllers.js
@@ -2,44 +2,47 @@ import ShortUrl from "../models/shortUrl.modes.js";
 import { nanoid } from "nanoid";
 
 const createShortUrl = async (req, res) => {
-   
-        const { url } = req.body;
 
-        if (!url) {
-            return res.status(400).json({ message: "URL is required" });
+    const { url } = req.body;
+
+    if (!url) {
+        return res.status(400).json({ message: "URL is required" });
+    }
+
+    try {
+        const short_url = nanoid(7);
+        const newUrl = await ShortUrl.create({
+            full_url: url,
+            short_url: short_url,
+        });
+
+        if (!newUrl) {
+            return res.status(400).json({ message: "Failed to create short URL" });
         }
 
-        try {
-            const short_url = nanoid(7);
-            const newUrl = await ShortUrl.create({ 
-                full_url:url,
-                short_url:short_url,
-            });
-
-            if(!newUrl){
-                return res.status(400).json({ message: "Failed to create short URL" });
-            }
-
-            return res.status(201).json(newUrl);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ message: "Internal server error" });
-        }
+        return res.status(201).json(newUrl);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 
 
-   
+
 };
 
 
 
 const getAllShortUrls = async (req, res) => {
+
+    const { id } = req.params;
     try {
-        const shortUrls = await ShortUrl.find();
-
-
-
+        const url = await ShortUrl.find({ short_url: id });
         
-        res.status(200).json(shortUrls);
+        if (url && url.length > 0) {
+            return res.redirect(url[0].full_url);
+        } else {
+            return res.status(404).json({ message: "URL not found" });
+        }
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal server error" });
--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.get("/",(req,res) => {
 })
 
 app.post("/api/create",createShortUrl);
-// app.get("/shorturl",getAllShortUrls);
+app.get("/:id",getAllShortUrls);
 
 
 
